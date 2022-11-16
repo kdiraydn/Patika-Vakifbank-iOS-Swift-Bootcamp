dcdace3c32f770989adfd85aece747d37cebb02c
kdiraydn <65663370+kdiraydn@users.noreply.github.com>
2022-11-16 22:00:11 +0300

    basicDataPersistance

---
+++ BasicDataPersistance/BasicDataPersistance/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BasicDataPersistance
+//
+//  Created by Abdulkadir Aydın on 13.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var userdefaultValueTextField: UILabel!
+    
+    @IBOutlet weak var userdefaultsValueTextField: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func saveUserdefaultsStringValue(_ sender: Any) {
+    }
+}
+
